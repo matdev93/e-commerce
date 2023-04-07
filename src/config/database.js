cd176bbcce2b66101cd56043b6719f2704a61cee
Cedric Noriega <70352411+CNoriegaTL@users.noreply.github.com>
2023-04-07 00:27:48 -0400

    Se agrega carpeta backend y se eliminan archivos originales

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.set('strictQuery', true); //* Linea opcional, en caso de que lance una advertencia
-
-mongoose
-  .connect(process.env.MONGODB)
-  .then(() => console.log('Base de datos conectado con exito'));
